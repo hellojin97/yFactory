113e3d37245f246aa264a5b79c10cf2ca0c59de5
seaij591 <seaij591@gmail.com>
2022-05-16 14:55:29 +0900

    MIN : 패키지 수정 2022.05.16

--- MES/src/main/java/com/yfactory/mes/prod/serviceImpl/ProdServiceImpl.java
+++ MES/src/main/java/com/yfactory/mes/prod/serviceImpl/ProdServiceImpl.java
@@ -1,7 +1,10 @@
 package com.yfactory.mes.prod.serviceImpl;
 
+import org.springframework.stereotype.Service;
+
 import com.yfactory.mes.prod.service.ProdService;
 
+@Service
 public class ProdServiceImpl implements ProdService {
 
 }

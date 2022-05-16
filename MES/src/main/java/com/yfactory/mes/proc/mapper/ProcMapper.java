27263fa1f73d81e96297a64ee9b6e624ea3c541f
rlatapud <kwkwguk1@gmail.com>
2022-05-16 16:34:31 +0900

    PJY :: 20220516 MAP 수정

--- MES/src/main/java/com/yfactory/mes/proc/mapper/ProcMapper.java
+++ MES/src/main/java/com/yfactory/mes/proc/mapper/ProcMapper.java
@@ -1,13 +1,22 @@
 package com.yfactory.mes.proc.mapper;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
-import com.yfactory.mes.test.TestVO;
 
 public interface ProcMapper {
-	List<TestVO> OrderSelect();		//주문서 등록
-	int PlanInsert(TestVO test);	//계획 등록
-	TestVO OrderSelectOne(TestVO ordCd);	//주문서 조회
 	
+	List<Map> OrderSelect();		//주문서 목록 조회
+	
+	int PlanInsert(TestVO test);	//계획 등록
+	
+	TestVO OrderSelectOne(TestVO ordCd);	//주문서 조회 (주문코드O)
+	TestVO OrderSelectOne(String prodCd);	//주문서 조회 (주문코드X)
+	
+	List<Map> PrdSelect();		//제품 목록 조회
+	List<Map> prdtPlanSelect(Date date1, Date date2);	//생산계획 조회
+	
+	int prdtPlanInsert(TestVO PP_NM, Date date1);
 	
 }

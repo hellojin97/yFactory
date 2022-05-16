b1929fd23509c28d66cc1c537d790474ee953994
LeeWoongJae <dsa00340@naver.com>
2022-05-16 14:31:31 +0900

    LWJ > service + serviceImpl 추가

--- MES/src/main/java/com/yfactory/mes/eq/mapper/EquipMapper.java
+++ MES/src/main/java/com/yfactory/mes/eq/mapper/EquipMapper.java
@@ -5,8 +5,12 @@ import java.util.List;
 import com.yfactory.mes.eq.vo.EqVO;
 
 public interface EquipMapper {
+	
 	/*
-	 * public List<EqVO> findAll(); // 설비 전체 조회 
-	 * public EqVO findSelect(String	 eq_cd); // 설비 낱개 조회
-	 */
+	 * public List<EqVO> findAll(); // 설비 전체 조회 public EqVO findSelect(String
+	 * eq_cd); // 설비 낱개 조회
+	 */	  
+	  
+	  
+	 
 	}

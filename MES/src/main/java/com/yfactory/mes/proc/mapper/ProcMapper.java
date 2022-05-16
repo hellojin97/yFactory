4bb23153d296d542f0e4fc09eec96a2f3cff0904
hyeonjink <guswls0975@naver.com>
2022-05-16 17:55:03 +0900

    KHJ : 22.05.16 AJAX 충돌 해결

--- MES/src/main/java/com/yfactory/mes/proc/mapper/ProcMapper.java
+++ MES/src/main/java/com/yfactory/mes/proc/mapper/ProcMapper.java
@@ -1,6 +1,10 @@
 package com.yfactory.mes.proc.mapper;
 
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+
 import com.yfactory.mes.proc.vo.PlanDtlVO;
 
 
--- MES/src/main/java/com/yfactory/mes/sales/web/SalesController.java
+++ MES/src/main/java/com/yfactory/mes/sales/web/SalesController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.yfactory.mes.sales.service.SalesService;
 
 @Controller
-public class PageController {
+public class SalesController {
 
 	@Autowired private SalesService salesService;
 	

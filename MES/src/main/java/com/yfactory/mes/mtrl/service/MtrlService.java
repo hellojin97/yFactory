4d88f557adfa7e06a1012d0939e0b3f4ffd4afdf
seaij591 <seaij591@gmail.com>
2022-05-16 17:28:16 +0900

    MIN : 발주 수정 2022.05.16

--- MES/src/main/java/com/yfactory/mes/mtrl/service/MtrlService.java
+++ MES/src/main/java/com/yfactory/mes/mtrl/service/MtrlService.java
@@ -1,10 +1,9 @@
 package com.yfactory.mes.mtrl.service;
 
 import java.util.List;
-
-import com.yfactory.mes.proc.vo.PrdtPlVO;
+import java.util.Map;
 
 public interface MtrlService {
 
-	List<PrdtPlVO>selectPl(PrdtPlVO vo);
+	List<Map>selectPl();
 }
--- MES/src/main/java/com/yfactory/mes/mtrl/serviceImpl/MtrlServiceImpl.java
+++ MES/src/main/java/com/yfactory/mes/mtrl/serviceImpl/MtrlServiceImpl.java
@@ -1,23 +1,24 @@
 package com.yfactory.mes.mtrl.serviceImpl;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.yfactory.mes.mtrl.mapper.MtrlMapper;
 import com.yfactory.mes.mtrl.service.MtrlService;
-import com.yfactory.mes.proc.vo.PrdtPlVO;
 
 @Service
 public class MtrlServiceImpl implements MtrlService {
 	
 	@Autowired MtrlMapper map;
-	
+
 	@Override
-	public List<PrdtPlVO> selectPl(PrdtPlVO vo) {
-		
-		return map.selectPl(vo);
+	public List<Map> selectPl() {
+		// TODO Auto-generated method stub
+		return map.selectPl();
 	}
+	
 
 }

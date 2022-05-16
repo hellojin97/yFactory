1e5fb4a24ff8232c8b543d9638c686d21ca5f26b
JinHwan <kerl6632@gmail.com>
2022-05-16 14:55:02 +0900

    kjh: ServiceImpl에 어노테이션 Service 추가

--- MES/src/main/java/com/yfactory/mes/quality/serviceImpl/QualityServiceImpl.java
+++ MES/src/main/java/com/yfactory/mes/quality/serviceImpl/QualityServiceImpl.java
@@ -1,7 +1,10 @@
 package com.yfactory.mes.quality.serviceImpl;
 
+import org.springframework.stereotype.Service;
+
 import com.yfactory.mes.quality.service.QualityService;
 
+@Service
 public class QualityServiceImpl implements QualityService {
 
 }

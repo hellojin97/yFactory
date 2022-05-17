be51ba6d8bd665cc31e79675e5d16a8994feafa8
LeeWoongJae <dsa00340@naver.com>
2022-05-17 16:49:57 +0900

    LWJ > 사이드바 하위 카테고리 추가 및 컨트롤러부 정의 20220517

---
+++ MES/src/main/java/com/yfactory/mes/eq/web/pageController.java
@@ -0,0 +1,53 @@
+package com.yfactory.mes.eq.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yfactory.mes.eq.mapper.EquipMapper;
+
+@Controller
+public class pageController {
+	// 페이지 부분 정의 컨트롤러
+	
+	@Autowired
+	public EquipMapper mapper;
+	
+	
+	@RequestMapping("/eqInsert")
+	public String eqInsert(){
+		// 설비 페이지  - 설비등록
+		return "eq/eqInsert";
+		
+	}
+	
+	@RequestMapping("/eqMng")
+	public String eqMng(){
+		// 설비 페이지 - 설비관리
+		return "eq/eqMng";
+		
+	}
+	
+	@RequestMapping("/eqChkMng")
+	public String eqChkMng(){
+		// 설비 페이지 - 점검관리
+		return "eq/eqChkMng";
+		
+	}
+	
+	@RequestMapping("/eqIna")
+	public String eqIna(){
+		// 설비 페이지 - 비가동 관리
+		return "eq/eqIna";
+		
+	}
+	
+	@RequestMapping("/eqRstat")
+	public String eqRstat(){
+		// 설비 페이지 - 실시간 설비 상태
+		return "eq/eqRstat";
+		
+	}
+	
+	
+}

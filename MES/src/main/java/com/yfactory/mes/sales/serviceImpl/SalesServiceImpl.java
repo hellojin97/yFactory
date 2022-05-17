0e0c7d79761b5615581c04ff28f662924ee45a1e
박진영 <pjy0302a@naver.com>
2022-05-17 16:44:19 +0900

    KSM : controller 및 impl 추가 22.05.17

--- MES/src/main/java/com/yfactory/mes/sales/serviceImpl/SalesServiceImpl.java
+++ MES/src/main/java/com/yfactory/mes/sales/serviceImpl/SalesServiceImpl.java
@@ -21,4 +21,9 @@ public class SalesServiceImpl implements SalesService {
 		return map.selectOrderList();
 	}
 
+	@Override
+	public List<Map> searchOrderList(String pnm, String vnm, String req1, String req2, String res1, String res2) {
+		return map.searchOrderList(pnm, vnm, req1, req2, res1, res2);
+	}
+
 }

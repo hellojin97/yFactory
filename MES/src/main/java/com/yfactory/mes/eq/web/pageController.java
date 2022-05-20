e160d926b110da439ee6045d37e74f82f58e4473
LeeWoongJae <dsa00340@naver.com>
2022-05-20 12:06:49 +0900

    LWJ > 이미지 경로 numeric error 수정진행중

--- MES/src/main/java/com/yfactory/mes/eq/web/pageController.java
+++ MES/src/main/java/com/yfactory/mes/eq/web/pageController.java
@@ -1,15 +1,10 @@
 package com.yfactory.mes.eq.web;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.yfactory.mes.eq.mapper.EquipMapper;
@@ -22,8 +17,8 @@ public class pageController {
 	@Autowired
 	public EquipMapper mapper;
 	
-	@Autowired
-	private String saveDir;
+//	@Autowired
+//	private String saveDir;
 	 
 	@RequestMapping("/eqInsert")
 	public String eqInsert(){
@@ -34,32 +29,36 @@ public class pageController {
 	
 	@RequestMapping("/eqMng")
 	public String eqMng(EqVO vo , MultipartFile file , Model model){
-		if(file.isEmpty()) { // 파일이 비어있다면 TRUE
-			mapper.insertEq(vo); // 비어있다면 DB에 저장을 시도
-		}else { // 비어있지 않다면
-			String originFileName = file.getOriginalFilename();
-			String saveFileName =UUID.randomUUID().toString()+originFileName.substring(originFileName.lastIndexOf('.'));
-			
-			
-			
-			try {
-				file.transferTo(new File(saveDir, saveFileName));// 물리적 위치에 저장
-				
-				saveFileName = saveDir + saveFileName; // 저장된 물리 경로를 포함
-				
-				// DB저장
-				vo.setUuid(saveFileName);
-				//vo.setFileName(originFileName);
-				//vo.setUuidFile(saveFileName);
-				mapper.insertEq(vo);
-
-					} catch (IllegalStateException | IOException e) {
-						e.printStackTrace();
-						} 
-					}
-		System.out.println("--------------------------");
-		System.out.println(vo.getUuid());
-		model.addAttribute("src" , vo.getUuid());
+	
+		//System.out.println(vo);
+		mapper.insertEq(vo); 
+		
+//		if(file.isEmpty()) { // 파일이 비어있다면 TRUE
+////			mapper.insertEq(vo); // 비어있다면 DB에 저장을 시도
+//		}else { // 비어있지 않다면
+//			String originFileName = file.getOriginalFilename();
+//			String saveFileName =UUID.randomUUID().toString()+originFileName.substring(originFileName.lastIndexOf('.'));
+//			
+//			
+//			
+//			try {
+//				file.transferTo(new File(saveDir, saveFileName));// 물리적 위치에 저장
+//				
+//				saveFileName = saveDir + saveFileName; // 저장된 물리 경로를 포함
+//				
+//				// DB저장
+//				vo.setUuid(saveFileName);
+//				//vo.setFileName(originFileName);
+//				//vo.setUuidFile(saveFileName);
+//				mapper.insertEq(vo);
+//
+//					} catch (IllegalStateException | IOException e) {
+//						e.printStackTrace();
+//						} 
+//					}
+//		System.out.println("--------------------------");
+//		System.out.println(vo.getUuid());
+//		model.addAttribute("src" , vo.getUuid());
 		return "eq/eqMng"; // 다 처리 하고 설비관리로 가라 
 	}
 	
